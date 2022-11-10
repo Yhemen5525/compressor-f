dacbd6248f0183c1e9bc402c250daf5d2797136b
igotapps650 <igotapps650@gmail.com>
2022-11-10 03:45:23 +0000

    initial commit

---
+++ compressors.js
@@ -0,0 +1,16 @@
+var product_db = TAFFY([
+  { "item"  : 1,
+    "name"  : "Blue Ray Player",
+    "price" : 99.99
+  },
+  { "item"  : 2,
+    "name"  : "3D TV",
+    "price" : 1799.99
+  }
+]);
+
+/**
+ * 
+mongodb+srv://abraham:<password>@cluster0.shjdy6i.mongodb.net/?retryWrites=true&w=majority
+
+*/
